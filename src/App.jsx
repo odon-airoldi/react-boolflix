f9aa72458fb7b9899ee1ebbf65a23fd68cc73f14
odon-airoldi <odon.airoldi@gmail.com>
2026-03-26 12:26:41 +0100

    add render search results

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,16 @@ function App() {
 
   const API_KEY = import.meta.env.VITE_API_KEY
 
-  const API_URL = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=ritorno`
+  const [renderSearchResult, setRenderSearchResult] = useState('')
 
-  const [movieSearchResult, setMovieSearchResult] = useState(null)
 
-  const [searchInput, setSearchInput] = useState('')
+  const [searchResult, setSearchResult] = useState(null)
+
+  const [queryInput, setQueryInput] = useState('')
+
+
+  const API_URL = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=${queryInput}`
+
 
   useEffect(() => {
 
@@ -16,30 +21,43 @@ function App() {
       .then(res => res.json())
       .then(data => {
 
-        setMovieSearchResult(data)
+        setSearchResult(data)
+
 
       })
 
-  }, [])
+  }, [queryInput])
 
   // console.log(movieSearchResult)
+
+
+
 
   function searchSubmit(e) {
 
     e.preventDefault()
 
-    console.log('submit form')
+    setRenderSearchResult(searchResult)
 
   }
+
+  console.log(renderSearchResult)
 
   return (
     <>
 
       <form onSubmit={searchSubmit}>
-        <input type="text" value={searchInput} onChange={(e) => setSearchInput(e.target.value)} />
-        {searchInput}
+        <input type="text" value={queryInput} onChange={(e) => setQueryInput(e.target.value)} />
         <button type="submit">Search</button>
       </form>
+
+      <div>
+        {
+          renderSearchResult?.results?.map(movie => (
+            <div>{movie.title}</div>
+          ))
+        }
+      </div>
 
     </>
   )
